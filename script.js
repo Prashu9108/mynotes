ece315ae85456cfdc4901fa0df1706355fa77c33
Prashu9108 <prashu9108@gmail.com>
2023-11-30 22:07:43 +0530

    black theme is implemented

--- script.js
+++ script.js
@@ -14,7 +14,34 @@ function dialogOpener (){
         dialog.style.display = "none"
         icon.classList.remove('fa-close');
         icon.classList.add('fa-plus')
+    }
+}
 
+var isLightTheme = true;
+const themeChanger = () => {
+    isLightTheme = !isLightTheme;
+    const nav = document.getElementById("nav")
+    const topbar = document.getElementById("top-bar")
+    const themeIcon = document.getElementById("theme");
+
+    if (isLightTheme) {
+        themeIcon.classList.remove("fa-cloud-moon");
+        themeIcon.classList.add("fa-cloud-sun");
+        document.body.style.background = "#fff";
+        document.body.style.color = "#000";
+        
+        //nav
+        nav.style.backgroundColor = "#fff";
+        topbar.style.backgroundColor = "#fff";
+
+    }
+    else {
+        themeIcon.classList.add("fa-cloud-moon");
+        themeIcon.classList.remove("fa-cloud-sun");
+        document.body.style.background = "#000";
+        document.body.style.color = "#fff";
+        nav.style.backgroundColor = "#252525";
+        topbar.style.backgroundColor = "#252525";
 
     }
 }
